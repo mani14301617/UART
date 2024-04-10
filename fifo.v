b7c192e0b6a6c5daa6f47597e506067d5b3585d7
mani14301617 <122259617+mani14301617@users.noreply.github.com>
2024-04-11 01:05:32 +0530

    Update fifo.v

--- fifo.v
+++ fifo.v
@@ -1,43 +1,64 @@
-module FIFO
-    #(parameter DATA_WIDTH=8,SIZE=16)
-    (
-        input clk,reset,
-        input rd_en,wr_en,
-        input [DATA_WIDTH-1:0] din,
-        output [DATA_WIDTH-1:0] dout,
-        output full,empty
-    );
-    
-    reg [DATA_WIDTH-1:0] memory [0:SIZE-1];
-    reg [$clog2(SIZE)-1:0] wr_ptr,rd_ptr;
-    reg [$clog2(SIZE)-1:0] number_of_elements;
-    
-    always @(posedge clk,negedge reset)
-    begin
-        if(~reset)
+module fifo #(
+    parameter DATA_WIDTH = 8, // Width of data in bits
+    parameter FIFO_DEPTH = 7 // Number of entries in FIFO
+)(
+    input wire clk, // Clock signal
+    input wire reset, // Reset signal, active low
+    input wire wr_en, // Write enable
+    input wire rd_en, // Read enable
+    input wire [DATA_WIDTH-1:0] wr_data, // Data to write
+    output reg [DATA_WIDTH-1:0] rd_data, // Data to read
+    output wire fifo_full, // FIFO full flag
+    output wire fifo_empty // FIFO empty flag
+);
+
+// Internal parameters
+localparam ADDR_WIDTH = $clog2(FIFO_DEPTH);
+
+// FIFO storage
+reg [DATA_WIDTH-1:0] memory [0:FIFO_DEPTH-1];
+
+// Read and write pointers
+reg [ADDR_WIDTH-1:0] wr_ptr, rd_ptr;
+
+// FIFO count to keep track of the number of items
+reg [ADDR_WIDTH :0] fifo_count;
+
+assign fifo_full = (fifo_count == FIFO_DEPTH);
+assign fifo_empty = (fifo_count == 0);
+
+  always @(posedge clk or negedge reset) begin
+    if (~reset) begin
+        wr_ptr <= 0;
+        rd_ptr <= 0;
+        fifo_count <= 0;
+    end
+    else begin
+    //rd_data <= memory[rd_ptr];
+      if(!fifo_empty)
         begin
-            rd_ptr <=0;
-            wr_ptr <=0;
-            number_of_elements <=0;
+          rd_data = memory[rd_ptr];
+          if(rd_en)
+            begin
+             rd_ptr = (rd_ptr + 1) % FIFO_DEPTH;
+             fifo_count = fifo_count - 1;
+            end
         end
-        else
-            begin
-            if(rd_en)
-            begin
-                rd_ptr <= rd_ptr + 1;
-                number_of_elements <= number_of_elements -1;
-            end
-            if(wr_en)
-            begin
-                memory[wr_ptr] <= din;
-                wr_ptr <= wr_ptr+1;
-                number_of_elements <= number_of_elements +1;
-            end
+        if (wr_en && !fifo_full) begin
+          //rd_data <= memory[rd_ptr];
+          memory[wr_ptr] = wr_data;
+         // rd_data <= memory[rd_ptr];
+            wr_ptr = (wr_ptr + 1) % FIFO_DEPTH;
+            fifo_count = fifo_count + 1;
+        end
+        
+       /* if (rd_en && !fifo_empty) begin
+            rd_data <= memory[rd_ptr];
+            rd_ptr <= (rd_ptr + 1) % FIFO_DEPTH;
+            fifo_count <= fifo_count - 1;
+        end*/
         end
     end
-    
-    assign dout = memory[rd_ptr];
-    assign empty = (rd_ptr==wr_ptr);
-    assign full = (number_of_elements==SIZE);
-    
+
+
 endmodule
