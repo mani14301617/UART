20024e930d95c696cd016c9a4aed822244a2f206
mani14301617 <122259617+mani14301617@users.noreply.github.com>
2024-04-11 01:09:54 +0530

    Update uart.v

--- uart.v
+++ uart.v
@@ -1,93 +1,21 @@
-module UART(
-        input [7:0] din,
-        input wr_uart,
-        input clk,reset,
-        input rd_uart,
-        output [7:0]out
-    );
-    
-    localparam FINAL_VALUE = 853;
-    wire [7:0] dout,dout2,dout3;
-    wire tx_done_tick,rx_done_tick;
-    wire empty_tx,full_tx,empty_rx,full_rx,s_tick;
-    wire rx;
-    
-    FIFO 
-    #(
-        .DATA_WIDTH(8),
-        .SIZE(16)
-    )
-    F_TX
-    (
-        .din(din),
-        .empty(empty_tx),
-        .full(full_tx),
-        .clk(clk),
-        .reset(reset),
-        .dout(dout),
-        .rd_en(tx_done_tick),
-        .wr_en(wr_uart)
-    );
-    FIFO 
-    #(
-        .DATA_WIDTH(8),
-        .SIZE(16)
-    )
-    F_RX
-    (
-        .din(dout2),
-        .empty(empty_rx),
-        .full(full_rx),
-        .clk(clk),
-        .reset(reset),
-        .dout(dout3),
-        .rd_en(rd_uart),
-        .wr_en(rx_done_tick)
-    );
-    
-    baud_rate_generator 
-    #(
-        .FINAL_VALUE_BITS($clog2(FINAL_VALUE))
-     )
-    B
-    (
-        .FINAL_VALUE(FINAL_VALUE),
-        .clk(clk),
-        .reset(reset),
-        .done(s_tick)
-    );
-    
-    Transmitter 
-    #(
-        .DATA_WIDTH(8),
-        .TIME(16)
-    )
-    T
-    (
-        .tx_start(~empty_tx),
-        .din(dout),
-        .clk(clk),
-        .s_tick(s_tick),
-        .reset(reset),
-        .tx(rx),
-        .tx_done_tick(tx_done_tick)
-    );
-    
-    Reciever
-    #(
-        .DATA_WIDTH(8),
-        .TIME(16)
-    )
-    R
-    (
-        .clk(clk),
-        .reset(reset),
-        .rx(rx),
-        .s_tick(s_tick),
-        .rx_done_tick(rx_done_tick),
-        .rx_out(dout2)
-    );
-    
-    assign out = dout3;
-    
+`timescale 1ns / 1ps
+module uart#(parameter nbits=8,stpbits=2)(rrd_en,twr_en,twr_data,rrd_data,tx_full,rx_full,rx_empty,clk,reset);
+  input rrd_en,twr_en,clk,reset;
+  input  [nbits-1:0]twr_data;
+  output [nbits-1:0]rrd_data;
+  output tx_full,rx_full,rx_empty;
+  
+  wire trd_en,rwr_en;
+  wire [nbits-1:0]trd_data,rwr_data;
+  wire tx_empty;
+  wire bridge;
+  fifo t_f(.clk(clk),.reset(reset),.wr_en(twr_en),.wr_data(twr_data),.rd_en(trd_en),.rd_data(trd_data),.fifo_full(tx_full),.fifo_empty(tx_empty));
+  
+  
+  transmitter t(.clk(clk),.reset(reset),.tx(bridge),.tx_din(trd_data),.tx_start(~tx_empty),.tx_done(trd_en));
+  
+  reciever r(.clk(clk),.reset(reset),.rx(bridge),.rx_dout(rwr_data),.rx_done(rwr_en));
+  
+  fifo r_f(.clk(clk),.reset(reset),.wr_en(rwr_en),.wr_data(rwr_data),.rd_en(rrd_en),.rd_data(rrd_data),.fifo_full(rx_full),.fifo_empty(rx_empty));
+  
 endmodule
